3702623bae7284f2ca91c1dfb0bfc9e9c3463f32
0xc0c0 <0xc0c0@benslab.org>
2022-01-22 10:38:54 -0600

    Completed Day 16

--- day16/solve.py
+++ day16/solve.py
@@ -1,7 +1,11 @@
+from binascii import hexlify, unhexlify
+import errno
 import logging
 import numpy as np
 import os
 import math
+from dataclasses import dataclass
+from typing import List
 
 logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
 logger = logging.getLogger(__name__)
@@ -12,19 +16,145 @@ def get_file_data(fn='input.txt'):
     return data
 
 def parse_data(text_data):
-    data = [[int(x) for x in list(line)] for line in text_data.strip('\n').strip().split('\n')]
-    return np.array(data)
+    hex_data = list(text_data.strip('\n').strip())
+    binary_stream = "".join([f"{int(b, 16):0>4b}" for b in hex_data])
+    return binary_stream
+
+@dataclass
+class Packet:
+    version: int = -1
+    type: int = -1
+    value: int = -1
+    
+@dataclass
+class OpPacket(Packet):
+    op_type: int = -1
+    op_len: int = -1
+    op_subpackets: list = None
+
+def decode_int(stream=None, bitlen=4, offset=0):
+    if not stream or not (len(stream) >= bitlen):
+        raise ValueError(errno)
+    
+    value = int(stream[offset:offset+bitlen], 2)
+    if offset == 0:
+        remaining_stream = stream[bitlen:]
+    else:
+        remaining_stream = None
+
+    return value, remaining_stream
+
+def decode_packet_header(stream=None, packet=None):
+    if packet == None:
+        packet = Packet()
+    # logger.debug(f"Packet Stream: {stream}")
+    packet.version, stream = decode_int(stream, 3)
+    packet.type, stream = decode_int(stream, 3)
+    # logger.debug(packet)
+    return packet, stream
+
+def decode_literal_packet(stream=None, packet=None):
+    packet, stream = decode_packet_header(stream)
+
+    more_bits, stream = decode_int(stream, 1)
+    val, stream = decode_int(stream, 4)
+    vals = [val]
+    while more_bits and len(stream) >= 5:
+        more_bits, stream = decode_int(stream, 1)
+        val, stream = decode_int(stream, 4)
+        vals.append(val)
+    val_bits = "".join([f"{i:0>4b}" for i in vals])
+    packet.value = int(val_bits, 2)
+    logger.debug(f"Decoded Literal Value Packet: {packet}")
+    return packet, stream
+
+def decode_operator_packet(stream, packet=None):
+    packet, stream = decode_packet_header(stream, packet=OpPacket())
+
+    packet.op_type, stream = decode_int(stream, 1)
+    packet.op_subpackets = list()
+    if packet.op_type == 0:
+        packet.op_len, stream = decode_int(stream, 15)
+        substream = stream[:packet.op_len]
+        stream = stream[packet.op_len:]
+        logger.debug(f"OpPacket substream: {substream}")
+        remaining_op_bits = packet.op_len
+        while remaining_op_bits > 0:
+            saved_len = len(substream)
+            p, substream = decode_packet(substream)
+            remaining_op_bits = saved_len - len(substream)
+            if p:
+                packet.op_subpackets.append(p)
+            else:
+                break 
+        
+    elif packet.op_type == 1:
+        packet.op_len, stream = decode_int(stream, 11)
+        remaining_op_packets = packet.op_len
+        while remaining_op_packets > 0:
+            p, stream = decode_packet(stream)
+            remaining_op_packets -= 1
+            if p:
+                packet.op_subpackets.append(p)
+            else:
+                break
+            
+    t = packet.type
+
+    if t == 0:
+        packet.value = sum([p.value for p in packet.op_subpackets])
+    elif t == 1:
+        packet.value = math.prod([p.value for p in packet.op_subpackets])
+    elif t == 2:
+        packet.value = min([p.value for p in packet.op_subpackets])
+    elif t == 3:
+        packet.value = max([p.value for p in packet.op_subpackets])
+    elif t == 5:
+        packet.value = (packet.op_subpackets[0].value > packet.op_subpackets[1].value)
+    elif t == 6:
+        packet.value = (packet.op_subpackets[0].value < packet.op_subpackets[1].value)
+    elif t == 7:
+        packet.value = (packet.op_subpackets[0].value == packet.op_subpackets[1].value)
+
+    logger.debug(f"Decoded Operator Packet: {packet}")
+    return packet, stream        
+        
+def decode_packet(stream):
+    if len(stream) < 11:
+        return None, stream
+    
+    # look ahead for packet type
+    packet_type, _ = decode_int(stream, bitlen=3, offset=3)
+    
+    # literal value packet
+    if packet_type == 4:
+        return decode_literal_packet(stream)
+    # operator packet
+    else:
+        return decode_operator_packet(stream)
+
+def get_packet_version_sum(packet):
+    if isinstance(packet, OpPacket):
+        sum = packet.version
+        for p in packet.op_subpackets:
+            sum += get_packet_version_sum(p)
+        return sum
+        
+    elif isinstance(packet, Packet):
+        return packet.version
+    
 
 def main():
     logger.setLevel(level=logging.INFO)
     with open("input.txt") as f:
         data = f.read()
        
-    grid = parse_data(data)
-    answer = 0
-    logger.info(f"Puzzle1: <SUMMARY>: {answer}")
-    answer = 0
-    logger.info(f"Puzzle2: <SUMMARY>: {answer}")
+    packet_data = parse_data(data)
+    packet, _ = decode_packet(packet_data)
+    answer = get_packet_version_sum(packet)
+    logger.info(f"Puzzle1: Version Sum: {answer}")
+    answer = packet.value
+    logger.info(f"Puzzle2: Evaluated Packet Value: {answer}")
     
 if __name__ == '__main__':
     main()
--- day16/test_solve.py
+++ day16/test_solve.py
@@ -1,4 +1,3 @@
-from numpy.lib.function_base import gradient
 import pytest
 from .solve import *
 
@@ -8,12 +7,129 @@ def test_data():
         text = f.read()
     return text
 
-def test_parse_input(test_data):
-    grid = parse_data(test_data)
-    assert type(grid) == np.ndarray
-    assert grid[1,3] == 5
-    assert grid.ndim == 2
-    assert grid.shape == (10,10)
+@pytest.fixture
+def test1_data():
+    with open('test1.txt', 'r') as f:
+        text = f.read()
+    return text
+
+@pytest.fixture
+def test2_data():
+    with open('test2.txt', 'r') as f:
+        text = f.read()
+    return text
+
+@pytest.fixture
+def test3_data():
+    with open('test3.txt', 'r') as f:
+        text = f.read()
+    return text
+
+@pytest.fixture
+def test4_data():
+    with open('test4.txt', 'r') as f:
+        text = f.read()
+    return text
+
+def test_parse_input(test_data,
+                     test1_data,
+                     test2_data,
+                     test3_data,
+                     test4_data,):
+    packets = parse_data(test_data)
+    assert type(packets) == str
+    assert len(packets) == 4 * 6
+    
+    packets = parse_data(test1_data)
+    assert len(packets) == 4 * 18
+    
+    packets = parse_data(test2_data)
+    assert len(packets) == 4 * 26
+    
+    packets = parse_data(test3_data)
+    assert len(packets) == 4 * 28
+    
+    packets = parse_data(test4_data)
+    assert len(packets) == 4 * 30
   
-def test_all(test_data):
-    grid = parse_data(test_data)
+def test_all_part1(test_data,
+             test1_data,
+             test2_data,
+             test3_data,
+             test4_data,):
+    packet_data = parse_data(test_data)
+    packet, _ = decode_packet(packet_data)
+    assert packet.version == 6
+    assert packet.type == 4
+    assert packet.value == 2021
+    
+    packet_data = parse_data(test1_data)
+    packet, _ = decode_packet(packet_data)
+    assert packet.version == 4
+    assert packet.type != 4
+    assert len(packet.op_subpackets) == 1
+    p = packet.op_subpackets[0]
+    assert p.version == 1
+    assert p.type != 4
+    assert len(p.op_subpackets) == 1
+    p = p.op_subpackets[0]
+    assert p.version == 5
+    assert p.type != 4
+    assert len(p.op_subpackets) == 1
+    p = p.op_subpackets[0]
+    assert p.version == 6
+    assert p.type == 4
+    assert p.value != -1
+    
+    packet_data = parse_data(test2_data)
+    packet, _ = decode_packet(packet_data)
+    assert packet.op_len == 2
+    assert len(packet.op_subpackets) == 2
+    p1 = packet.op_subpackets[0]
+    assert p1.op_type == 0
+    assert p1.op_len == 22
+    assert len(p1.op_subpackets) == 2
+    p2 = packet.op_subpackets[1]
+    assert len(p2.op_subpackets) == 2
+    assert get_packet_version_sum(packet) == 12
+    
+    packet_data = parse_data(test3_data)
+    packet, _ = decode_packet(packet_data)
+    assert get_packet_version_sum(packet) == 23
+        
+    packet_data = parse_data(test4_data)
+    packet, _ = decode_packet(packet_data)
+    assert get_packet_version_sum(packet) == 31
+    
+def test_all_part2():
+    packet_data = parse_data('C200B40A82')
+    packet, _ = decode_packet(packet_data)
+    assert packet.value == 3
+    
+    packet_data = parse_data('04005AC33890')
+    packet, _ = decode_packet(packet_data)
+    assert packet.value == 54
+    
+    packet_data = parse_data('880086C3E88112')
+    packet, _ = decode_packet(packet_data)
+    assert packet.value == 7
+    
+    packet_data = parse_data('CE00C43D881120')
+    packet, _ = decode_packet(packet_data)
+    assert packet.value == 9
+    
+    packet_data = parse_data('D8005AC2A8F0')
+    packet, _ = decode_packet(packet_data)
+    assert packet.value == 1
+    
+    packet_data = parse_data('F600BC2D8F')
+    packet, _ = decode_packet(packet_data)
+    assert packet.value == 0
+    
+    packet_data = parse_data('9C005AC2F8F0')
+    packet, _ = decode_packet(packet_data)
+    assert packet.value == 0
+    
+    packet_data = parse_data('9C0141080250320F1802104A08')
+    packet, _ = decode_packet(packet_data)
+    assert packet.value == 1
