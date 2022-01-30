2e145105b9989d9caf68864408a00a1a4d96e745
0xc0c0 <0xc0c0@benslab.org>
2022-01-30 12:06:36 -0600

    Start of Day 18

---
+++ day18/solve.py
@@ -0,0 +1,28 @@
+import logging
+import numpy as np
+
+logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
+logger = logging.getLogger(__name__)
+
+def get_file_data(fn='input.txt'):
+    with open(fn) as f:
+        data = f.read()
+    return data
+
+def parse_data(text_data):
+    data = [[int(x) for x in list(line)] for line in text_data.strip('\n').strip().split('\n')]
+    return np.array(data)
+
+def main():
+    logger.setLevel(level=logging.INFO)
+    with open("input.txt") as f:
+        data = f.read()
+       
+    data = parse_data(data)
+    answer = 0
+    logger.info(f"Puzzle1: <SUMMARY>: {answer}")
+    answer = 0
+    logger.info(f"Puzzle2: <SUMMARY>: {answer}")
+    
+if __name__ == '__main__':
+    main()
