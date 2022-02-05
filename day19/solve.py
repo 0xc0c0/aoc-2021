171bbac99a80c6fd5bb4b2b2e0718501aac0d1be
0xc0c0 <0xc0c0@benslab.org>
2022-02-05 15:19:50 -0600

    WIP Day 19

--- day19/solve.py
+++ day19/solve.py
@@ -2,6 +2,7 @@ import logging
 import os
 import numpy as np
 import ast
+import pandas as pd
 
 logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
 logger = logging.getLogger(__name__)
@@ -14,31 +15,54 @@ def get_file_data(fn='input.txt'):
 def parse_data(text_data):
     
     blobs = [blob for blob in text_data.strip('\n').strip().split('\n\n')]
-    scanner_data = list()
-    for blob in blobs:
-        scans = [ast.literal_eval(f"[{line}]") for line in blob.split('\n')[1:]]
-        scanner_data.append(scans)
-    return scanner_data
+    #scanner_data = pd.DataFrame(None, columns=)
+    scan_data = [[scanner_index] + [line_index] + ast.literal_eval(f"[{line}]") 
+             for scanner_index, blob in enumerate(blobs) 
+             for line_index, line in enumerate(blob.split('\n')[1:])]
+    scans = pd.DataFrame(scan_data, columns=['scanner','beacon', 'x','y','z'])
+    scans = scans.set_index(['scanner', 'beacon'])
+    return scans
 
 def get_scanned_beacon_distances(scans):
-    lookup_distances_sq = dict()
-    for beacon1 in scans:
-        for beacon2 in scans:
-            key = set(beacon1, beacon2)
-            if beacon1 == beacon2 or key in lookup_distances_sq:
-                continue
-            #add distance calc
-            x1, y1, z1 = beacon1
-            x2, y2, z2 = beacon2
-            lookup_distances_sq[key] = (x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2
-    return lookup_distances_sq
+    dists_sq = pd.DataFrame([],columns=['scanner','beacon1','beacon2','distance_sq'])
+    dists_sq = dists_sq.set_index(['scanner','beacon1','beacon2'])
+    for scanner in scans.index.levels[0]:
+        scan = scans.loc[scanner]
+        for beacon1, point1 in scan.iterrows():
+            tmp = list()
+            for beacon2, point2 in scan[beacon1 + 1:].iterrows():
+                #add distance_sq calc
+                x1, y1, z1 = point1
+                x2, y2, z2 = point2
+                distance_sq = (x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2
+                tmp.append([scanner, beacon1, beacon2, distance_sq])
+            tmp_df = pd.DataFrame(data=tmp, columns=['scanner','beacon1','beacon2','distance_sq'])
+            tmp_df = tmp_df.set_index(['scanner','beacon1','beacon2'])
+            dists_sq = pd.concat([dists_sq, tmp_df])
+    logging.debug(dists_sq)
+    return dists_sq
 
-def get_overlapping_beacon_count(lu1, lu2):
-    for k1, v1 in enumerate(lu1):
-        for k2, v2 in enumerate(lu2):
-            if v1 == v2:
-                logger.info(f"{k1} looks like it could match {k2}")
-            
+def get_max_overlapping_beacons(dists_sq, base_index=0, min_count=12, ignore_scans=[]):
+    base_scan = dists_sq.loc[base_index]
+    match_count = 0
+    match_index = None
+    ignore_scans += [base_index]
+    for test_index in dists_sq.index.levels[0]:
+        if test_index in ignore_scans:
+            continue
+        
+        test_scan = dists_sq.loc[test_index]
+        eq_dist = base_scan.merge(test_scan, on=['distance_sq'], how='inner')
+        test_count = len(eq_dist)
+        if test_count > match_count:
+            match_count = test_count
+            match_index = test_index
+        logger.debug(f"scanners {base_index} and {test_index} had {test_count} matches")
+
+    return match_count, match_index
+
+def find_orientation(dists_sq, index1, index2, min_count=12):
+    pass
 
 def main():
     logger.setLevel(level=logging.INFO)
--- day19/test_solve.py
+++ day19/test_solve.py
@@ -8,10 +8,15 @@ def test_data():
     return text
 
 def test_parse_input(test_data):
-    scanner_data = parse_data(test_data)
-    assert len(scanner_data) == 5
-    assert scanner_data[2][0][0] == 649
-    assert scanner_data[4][2][2] == -461
+    scans = parse_data(test_data)
+    assert len(scans) == 127
+    assert scans.loc[2,0][0] == 649
+    assert scans.loc[4,2][2] == -461
   
 def test_all(test_data):
-    scanner_data = parse_data(test_data)
+    scans = parse_data(test_data)
+    dists_sq = get_scanned_beacon_distances(scans)
+    test_01 = (404-528) ** 2 + (643-588) ** 2 + (901+409) ** 2
+    assert (dists_sq.loc[0,0,1] == test_01).all()
+    
+    total, i = get_max_overlapping_beacons(dists_sq)
