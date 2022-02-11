a1cc2652682f447a231e9a201fe1ec43ef6aa214
0xc0c0 <0xc0c0@benslab.org>
2022-02-11 08:01:15 -0600

    Day 21 Complete

--- day21/solve.py
+++ day21/solve.py
@@ -17,27 +17,27 @@ def parse_data(text_data):
     lines = [line for line in text_data.strip('\n').strip().split('\n')]
     for line in lines:
         if 'Player 1' in line:
-            p1_start = int(line.split(':')[1].strip()) - 1
+            p1_start = int(line.split(':')[1].strip())
         else:
-            p2_start = int(line.split(':')[1].strip()) - 1
+            p2_start = int(line.split(':')[1].strip())
     return p1_start, p2_start
 
-def roll_dice(dice_state=-1, times=3):
+def roll_dice(dice_state=0, times=3):
     distance = 0
     for i in range(times):
-        dice_state = (dice_state + 1) % 100
-        distance += dice_state + 1
+        dice_state = (dice_state % 100) + 1
+        distance += dice_state
         # logger.debug(f"dice_state: {dice_state}, distance: {distance}")
     return dice_state, distance
 
 def turn(player_position, dice_state):
     dice_state, distance = roll_dice(dice_state)
-    player_position = (player_position + distance) % 10
-    score = player_position + 1
+    player_position = ((player_position + distance - 1) % 10) + 1
+    score = player_position
     return score, player_position, dice_state
 
 def run_game(p1, p2, winning_score=1000):
-    p1_score, p2_score, dice_state, rolls = 0, 0, -1, 0
+    p1_score, p2_score, dice_state, rolls = 0, 0, 0, 0
     times = 3
     winner, loser = 0, 0
     while p1_score < winning_score and p2_score < winning_score:
@@ -60,19 +60,19 @@ def run_game(p1, p2, winning_score=1000):
     return winner, loser, rolls
 
 def score(player_position, distance):
-    player_position = (player_position + distance) % 10
-    return player_position + 1, player_position
+    player_position = ((player_position + distance - 1) % 10) + 1
+    return player_position
 
 def run_quantum_game(p1_pos, p1_score, p2_pos, p2_score, p1_turn=True, winning_score=21):
     global GAMES
     global TRACE
     game_tuple = (p1_pos, p1_score, p2_pos, p2_score, p1_turn)
     
-    if logger.level <= logging.DEBUG:
-        logger.debug(game_tuple)
-        TRACE += 1
-        if TRACE == 10:
-            raise ValueError
+    # if logger.level <= logging.DEBUG:
+    logger.debug(game_tuple)
+    #     TRACE += 1
+    #     if TRACE == 10:
+    #         raise ValueError
     
     
     
@@ -80,51 +80,52 @@ def run_quantum_game(p1_pos, p1_score, p2_pos, p2_score, p1_turn=True, winning_s
         pass
     
     elif p1_score >= winning_score:
-        # logger.debug(f"set {game_tuple} to 1, 0")
+        logger.debug(f"set {game_tuple} to 1, 0")
         GAMES[game_tuple] = np.array([1, 0], dtype=np.int64)
         return GAMES[game_tuple]
 
     elif p2_score >= winning_score:
-        # logger.debug(f"set {game_tuple} to 0, 1")
-        GAMES[game_tuple] = np.array((1, 0), dtype=np.int64)
+        logger.debug(f"set {game_tuple} to 0, 1")
+        GAMES[game_tuple] = np.array([0, 1], dtype=np.int64)
         return GAMES[game_tuple]
     
     else:
         tallies = np.array([0, 0], dtype=np.int64)
         if p1_turn:
-            s, p = score(p1_pos, 3)
-            tallies += 1 * run_quantum_game(p, p1_score+s, p2_pos, p2_score, False)
-            s, p = score(p1_pos, 4)
-            tallies += 3 * run_quantum_game(p, p1_score+s, p2_pos, p2_score, False)
-            s, p = score(p1_pos, 5)
-            tallies += 6 * run_quantum_game(p, p1_score+s, p2_pos, p2_score, False)
-            s, p = score(p1_pos, 6)
-            tallies += 7 * run_quantum_game(p, p1_score+s, p2_pos, p2_score, False)
-            s, p = score(p1_pos, 7)
-            tallies += 6 * run_quantum_game(p, p1_score+s, p2_pos, p2_score, False)
-            s, p = score(p1_pos, 8)
-            tallies += 3 * run_quantum_game(p, p1_score+s, p2_pos, p2_score, False)
-            s, p = score(p1_pos, 9)
-            tallies += 1 * run_quantum_game(p, p1_score+s, p2_pos, p2_score, False)
+            p = score(p1_pos, 3)
+            logger.debug(f"distance of 3 from {p1_pos} yield a new position of {p}, score from {p1_score} --> {p1_score+p}")
+            tallies += 1 * run_quantum_game(p, p1_score+p, p2_pos, p2_score, False)
+            p = score(p1_pos, 4)
+            tallies += 3 * run_quantum_game(p, p1_score+p, p2_pos, p2_score, False)
+            p = score(p1_pos, 5)
+            tallies += 6 * run_quantum_game(p, p1_score+p, p2_pos, p2_score, False)
+            p = score(p1_pos, 6)
+            tallies += 7 * run_quantum_game(p, p1_score+p, p2_pos, p2_score, False)
+            p = score(p1_pos, 7)
+            tallies += 6 * run_quantum_game(p, p1_score+p, p2_pos, p2_score, False)
+            p = score(p1_pos, 8)
+            tallies += 3 * run_quantum_game(p, p1_score+p, p2_pos, p2_score, False)
+            p = score(p1_pos, 9)
+            tallies += 1 * run_quantum_game(p, p1_score+p, p2_pos, p2_score, False)
             GAMES[game_tuple] = tallies
             logger.debug(f"set {game_tuple} to {tallies}")
             
         else:
             # take this turn and explode all the universes
-            s, p = score(p2_pos, 3)
-            tallies += 1 * run_quantum_game(p1_pos, p1_score, p, p2_score+s, True)
-            s, p = score(p2_pos, 4)
-            tallies += 6 * run_quantum_game(p1_pos, p1_score, p, p2_score+s, True)
-            s, p = score(p2_pos, 5)
-            tallies += 6 * run_quantum_game(p1_pos, p1_score, p, p2_score+s, True)
-            s, p = score(p2_pos, 6)
-            tallies += 7 * run_quantum_game(p1_pos, p1_score, p, p2_score+s, True)
-            s, p = score(p2_pos, 7)
-            tallies += 6 * run_quantum_game(p1_pos, p1_score, p, p2_score+s, True)
-            s, p = score(p2_pos, 8)
-            tallies += 3 * run_quantum_game(p1_pos, p1_score, p, p2_score+s, True)
-            s, p = score(p2_pos, 9)
-            tallies += 1 * run_quantum_game(p1_pos, p1_score, p, p2_score+s, True)
+            p = score(p2_pos, 3)
+            tallies += 1 * run_quantum_game(p1_pos, p1_score, p, p2_score+p, True)
+            p = score(p2_pos, 4)
+            tallies += 3 * run_quantum_game(p1_pos, p1_score, p, p2_score+p, True)
+            p = score(p2_pos, 5)
+            tallies += 6 * run_quantum_game(p1_pos, p1_score, p, p2_score+p, True)
+            p = score(p2_pos, 6)
+            tallies += 7 * run_quantum_game(p1_pos, p1_score, p, p2_score+p, True)
+            p = score(p2_pos, 7)
+            tallies += 6 * run_quantum_game(p1_pos, p1_score, p, p2_score+p, True)
+            p = score(p2_pos, 8)
+            tallies += 3 * run_quantum_game(p1_pos, p1_score, p, p2_score+p, True)
+            p = score(p2_pos, 9)
+            tallies += 1 * run_quantum_game(p1_pos, p1_score, p, p2_score+p, True)
             logger.debug(f"set {game_tuple} to {tallies}")
             GAMES[game_tuple] = tallies
     
@@ -141,8 +142,10 @@ def main():
     winner, loser, rolls = run_game(p1, p2)
     answer = loser * rolls
     logger.info(f"Puzzle1: Dirac Dice, game to 1000: {answer}")
-    answer = 0
-    logger.info(f"Puzzle2: <SUMMARY>: {answer}")
+    p1, p2 = parse_data(data)
+    p1_wins, p2_wins = run_quantum_game(p1, 0, p2, 0)
+    answer = p1_wins if p1_wins > p2_wins else p2_wins
+    logger.info(f"Puzzle2: Dirac Dice Quantum Winner Count: {answer}")
     
 if __name__ == '__main__':
     main()
--- day21/test_solve.py
+++ day21/test_solve.py
@@ -9,28 +9,28 @@ def test_data():
 
 def test_parse_input(test_data):
     p1, p2 = parse_data(test_data)
-    assert p1 == 3
-    assert p2 == 7
+    assert p1 == 4
+    assert p2 == 8
   
 def test_all(test_data):
     p1, p2 = parse_data(test_data)
-    score, p1, dice = turn(p1, dice_state=-1)
+    score, p1, dice = turn(p1, dice_state=0)
     assert score == 10
-    assert p1 == 9
-    assert dice == 2
-    
-    
-    
+    assert p1 == 10
+    assert dice == 3
+     
     p1, p2 = parse_data(test_data)
     winner, loser, rolls = run_game(p1, p2)
     assert winner >= 1000
     assert loser == 745
     assert rolls == 993
 
-    # p1_wins, p2_wins = run_quantum_game(p1, 20, p2, 0)
-    # assert p1_wins == 444356092776315
-    # assert p2_wins == 341960390180808
+    p1, p2 = parse_data(test_data)
+    p1_wins, p2_wins = run_quantum_game(p1, 18, p2, 0)
+    assert p1_wins == 6579
+    assert p2_wins == 0
 
+    p1, p2 = parse_data(test_data)
     p1_wins, p2_wins = run_quantum_game(p1, 0, p2, 0)
     assert p1_wins == 444356092776315
     assert p2_wins == 341960390180808
